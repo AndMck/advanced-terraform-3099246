b8eba64c5fa33024fcdd6f392a5221a988a4e962
AndMck <mckeland@gmail.com>
2024-12-03 16:11:04 +0000

    First start

--- main.tf
+++ main.tf
@@ -1,125 +1,245 @@
 ### PROVIDER
-provider "google" {
-  project = "advancedterraform" #replace this with your project-id
-  region  = "us-central1"
-  zone    = "us-central1-a"
-}
+terraform {
+  backend "remote" {
+    organization = "Mactech-44"
 
-### NETWORK
-data "google_compute_network" "default" {
-  name                    = "default"
-}
-
-## SUBNET
-resource "google_compute_subnetwork" "subnet-1" {
-  name                     = "subnet1"
-  ip_cidr_range            = "10.127.0.0/20"
-  network                  = data.google_compute_network.default.self_link
-  region                   = "us-central1"
-  private_ip_google_access = true
-}
-
-resource "google_compute_firewall" "default" {
-  name    = "test-firewall"
-  network = data.google_compute_network.default.self_link
-
-  allow {
-    protocol = "icmp"
-  }
-
-  allow {
-    protocol = "tcp"
-    ports    = ["80", "8080", "1000-2000", "22"]
-  }
-
-  source_tags = ["web"]
-}
-
-### COMPUTE
-## NGINX PROXY
-resource "google_compute_instance" "nginx_instance" {
-  name         = "nginx-proxy"
-  machine_type = "f1-micro"
-  tags = ["web"]
-  
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-
-  network_interface {
-    network = data.google_compute_network.default.self_link
-    subnetwork = google_compute_subnetwork.subnet-1.self_link
-    access_config {
-      
+    workspaces {
+      name = "advanced-terraform-3099246"
     }
   }
 }
 
-## WEB1
-resource "google_compute_instance" "web1" {
-  name         = "web1"
-  machine_type = "f1-micro"
-  
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-
-  network_interface {
-    # A default network is created for all GCP projects
-    network = data.google_compute_network.default.self_link
-    subnetwork = google_compute_subnetwork.subnet-1.self_link
-  }
-}
-## WEB2
-resource "google_compute_instance" "web2" {
-  name         = "web2"
-  machine_type = "f1-micro"
-  
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-
-  network_interface {
-    network = data.google_compute_network.default.self_link
-    subnetwork = google_compute_subnetwork.subnet-1.self_link
-  }
-}
-## WEB3
-resource "google_compute_instance" "web3" {
-  name         = "web3"
-  machine_type = "f1-micro"
-  
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-
-  network_interface {
-    network = data.google_compute_network.default.self_link
-    subnetwork = google_compute_subnetwork.subnet-1.self_link
-  }  
+provider "azurerm" {
+  features {}
 }
 
-## DB
-resource "google_compute_instance" "mysqldb" {
-  name         = "mysqldb"
-  machine_type = "f1-micro"
-  
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
+# ### NETWORK
+# resource "azurerm_virtual_network" "default_vnet" {
+#   name                = "default-vnet"
+#   location            = "West Europe"
+#   resource_group_name = azurerm_resource_group.myrg-rg.name
+#   address_space       = ["10.127.0.0/16"]
 
-  network_interface {
-    network = data.google_compute_network.default.self_link
-    subnetwork = google_compute_subnetwork.subnet-1.self_link
-  }  
-}
+#   tags = {
+#     environment = "dev"
+#   }
+# }
+
+# ## SUBNET
+# resource "azurerm_subnet" "subnet_1" {
+#   name                 = "subnet1"
+#   resource_group_name  = azurerm_resource_group.myrg-rg.name
+#   virtual_network_name = azurerm_virtual_network.default_vnet.name
+#   address_prefixes     = ["10.127.0.0/20"]
+
+#   delegation {
+#     name = "subnetDelegation"
+#     service_delegation {
+#       name = "Microsoft.Network/virtualNetworks"
+#     }
+#   }
+# }
+
+# ### FIREWALL (NSG)
+# resource "azurerm_network_security_group" "default_nsg" {
+#   name                = "default-nsg"
+#   location            = azurerm_resource_group.myrg-rg.location
+#   resource_group_name = azurerm_resource_group.myrg-rg.name
+
+#   tags = {
+#     environment = "dev"
+#   }
+# }
+
+# resource "azurerm_network_security_rule" "allow_icmp_and_tcp" {
+#   name                        = "allow-icmp-and-tcp"
+#   priority                    = 100
+#   direction                   = "Inbound"
+#   access                      = "Allow"
+#   protocol                    = "*"
+#   source_port_range           = "*"
+#   destination_port_ranges     = ["80", "8080", "1000-2000", "22"]
+#   source_address_prefix       = "*"
+#   destination_address_prefix  = "*"
+#   resource_group_name         = azurerm_resource_group.myrg-rg.name
+#   network_security_group_name = azurerm_network_security_group.default_nsg.name
+# }
+
+# ### COMPUTE
+# ## BASE VARIABLES
+# variable "vm_size" {
+#   default = "Standard_B1s"
+# }
+
+# variable "image_reference" {
+#   default = {
+#     publisher = "Canonical"
+#     offer     = "UbuntuServer"
+#     sku       = "18.04-LTS"
+#     version   = "latest"
+#   }
+# }
+
+# ## NGINX PROXY
+# resource "azurerm_linux_virtual_machine" "nginx_instance" {
+#   name                = "nginx-instance"
+#   resource_group_name = azurerm_resource_group.myrg-rg.name
+#   location            = azurerm_resource_group.myrg-rg.location
+#   size                = var.vm_size
+
+#   admin_username      = "nginxadmin"
+#   network_interface_ids = [
+#     azurerm_network_interface.nginx_instance_nic.id
+#   ]
+
+#   admin_ssh_key {
+#     username   = "nginxadmin"
+#     public_key = var.ssh_public_key
+#   }
+
+#   os_disk {
+#     caching              = "ReadWrite"
+#     storage_account_type = "Standard_LRS"
+#   }
+
+#   source_image_reference {
+#     publisher = var.image_reference["publisher"]
+#     offer     = var.image_reference["offer"]
+#     sku       = var.image_reference["sku"]
+#     version   = var.image_reference["version"]
+#   }
+
+#   tags = {
+#     environment = "dev"
+#   }
+# }
+
+# resource "azurerm_network_interface" "nginx_instance_nic" {
+#   name                = "nginx-instance-nic"
+#   resource_group_name = azurerm_resource_group.myrg-rg.name
+#   location            = azurerm_resource_group.myrg-rg.location
+
+#   ip_configuration {
+#     name                          = "nginx-ip-config"
+#     subnet_id                     = azurerm_subnet.subnet_1.id
+#     private_ip_address_allocation = "Dynamic"
+#     public_ip_address_id          = azurerm_public_ip.nginx_instance_pip.id
+#   }
+
+#   tags = {
+#     environment = "dev"
+#   }
+# }
+
+# resource "azurerm_public_ip" "nginx_instance_pip" {
+#   name                = "nginx-instance-pip"
+#   resource_group_name = azurerm_resource_group.myrg-rg.name
+#   location            = azurerm_resource_group.myrg-rg.location
+#   allocation_method   = "Static"
+
+#   tags = {
+#     environment = "dev"
+#   }
+# }
+
+# ## WEB INSTANCES
+# resource "azurerm_linux_virtual_machine" "web_instances" {
+#   count               = 3
+#   name                = "web${count.index + 1}"
+#   resource_group_name = azurerm_resource_group.myrg-rg.name
+#   location            = azurerm_resource_group.myrg-rg.location
+#   size                = var.vm_size
+
+#   admin_username      = "webadmin"
+#   network_interface_ids = [
+#     azurerm_network_interface.web_instance_nics[count.index].id
+#   ]
+
+#   admin_ssh_key {
+#     username   = "webadmin"
+#     public_key = var.ssh_public_key
+#   }
+
+#   os_disk {
+#     caching              = "ReadWrite"
+#     storage_account_type = "Standard_LRS"
+#   }
+
+#   source_image_reference {
+#     publisher = var.image_reference["publisher"]
+#     offer     = var.image_reference["offer"]
+#     sku       = var.image_reference["sku"]
+#     version   = var.image_reference["version"]
+#   }
+
+#   tags = {
+#     environment = "dev"
+#   }
+# }
+
+# resource "azurerm_network_interface" "web_instance_nics" {
+#   count               = 3
+#   name                = "web-nic-${count.index + 1}"
+#   resource_group_name = azurerm_resource_group.myrg-rg.name
+#   location            = azurerm_resource_group.myrg-rg.location
+
+#   ip_configuration {
+#     name                          = "web-ip-config-${count.index + 1}"
+#     subnet_id                     = azurerm_subnet.subnet_1.id
+#     private_ip_address_allocation = "Dynamic"
+#   }
+
+#   tags = {
+#     environment = "dev"
+#   }
+# }
+
+# ## DATABASE INSTANCE
+# resource "azurerm_linux_virtual_machine" "mysql_instance" {
+#   name                = "mysql-instance"
+#   resource_group_name = azurerm_resource_group.myrg-rg.name
+#   location            = azurerm_resource_group.myrg-rg.location
+#   size                = var.vm_size
+
+#   admin_username      = "mysqladmin"
+#   network_interface_ids = [
+#     azurerm_network_interface.mysql_instance_nic.id
+#   ]
+
+#   admin_ssh_key {
+#     username   = "mysqladmin"
+#     public_key = var.ssh_public_key
+#   }
+
+#   os_disk {
+#     caching              = "ReadWrite"
+#     storage_account_type = "Standard_LRS"
+#   }
+
+#   source_image_reference {
+#     publisher = var.image_reference["publisher"]
+#     offer     = var.image_reference["offer"]
+#     sku       = var.image_reference["sku"]
+#     version   = var.image_reference["version"]
+#   }
+
+#   tags = {
+#     environment = "dev"
+#   }
+# }
+
+# resource "azurerm_network_interface" "mysql_instance_nic" {
+#   name                = "mysql-instance-nic"
+#   resource_group_name = azurerm_resource_group.myrg-rg.name
+#   location            = azurerm_resource_group.myrg-rg.location
+
+#   ip_configuration {
+#     name                          = "mysql-ip-config"
+#     subnet_id                     = azurerm_subnet.subnet_1.id
+#     private_ip_address_allocation = "Dynamic"
+#   }
+
+#   tags = {
+#     environment = "dev"
+#   }
+# }
